7ec44a3be9f741bbb46859dc0e75b782d252bf5d
Duane Johnson <duane.johnson@gmail.com>
2021-02-01 13:31:00 -0700

    Serve up blog posts on the backend

--- examples/server/server.ts
+++ examples/server/server.ts
@@ -1,11 +1,34 @@
-import * as express from "express";
-import { braidify } from "braidjs";
+import { SubscribableValue } from "braidjs";
 
-const app = express().use(braidify);
+import * as polka from "polka";
+import * as cors from "cors";
+
+const posts = [
+  { title: "hello." },
+  { title: "Welcome to\nThe Braid." },
+  {
+    title: "A microblog you own",
+    body:
+      `Braid isn't satisfied to be yet another proprietary platform ` +
+      `or protocol. We're working with the IETF to augment the HTTP ` +
+      `standard so that subscribing and collaborating is native to the web.`,
+  },
+  {
+    body:
+      `Share your life, and share how you see life. This microblog ` +
+      `supports tweaking and sharing attention functions--no longer ` +
+      `will you be subject to someone else's attention algorithm.`,
+  },
+];
+
+const blog = new SubscribableValue(posts);
+// published: new Date(Date.UTC(2021, 1, 1, 18, 30, 0)).toUTCString(),
+
+const app = polka().use(cors({ origin: true }));
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send(`The sedulous hyena ate the zookeeper!`);
+  blog.respond(req, res);
 });
 
 app.listen(port, (err) => {
