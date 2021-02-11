7ac253fa44dce4b82040996662493448f39e658c
Duane Johnson <duane.johnson@gmail.com>
2021-02-11 14:58:11 -0700

    Add 'yarn start-http2' to open on localhost with http2, tls

--- client/src/Settings/config.js
+++ client/src/Settings/config.js
@@ -1,5 +1,12 @@
 import { writable } from "svelte-local-storage-store";
 
+const DEFAULT_URL = import.meta.env["SNOWPACK_PUBLIC_URL"];
+
+const serverUrl = writable(
+  "serverUrl",
+  DEFAULT_URL || "https://invisible.college:4545"
+);
+
 export const config = {
-  serverUrl: writable("serverUrl", "https://braid.rphilosophy.com"),
+  serverUrl,
 };
