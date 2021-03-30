5436fd76bbba2b3a9a3aba274d940cc5a9d77d9a
Duane Johnson <duane.johnson@gmail.com>
2021-03-30 16:31:11 -0600

    Add describe block to utils test

--- server/tests/utils.test.ts
+++ server/tests/utils.test.ts
@@ -1,16 +1,18 @@
 import { send, error } from "../src/utils";
 import { getMockRes } from "@jest-mock/express";
 
-test("send", async () => {
-  const { res } = getMockRes();
-  send(res, []);
-  expect(res.statusCode).toEqual(200);
-  expect(res.end).toHaveBeenCalledWith("[]");
-});
+describe("utils", () => {
+  test("send", async () => {
+    const { res } = getMockRes();
+    send(res, []);
+    expect(res.statusCode).toEqual(200);
+    expect(res.end).toHaveBeenCalledWith("[]");
+  });
 
-test("error", async () => {
-  const { res } = getMockRes();
-  error(res, "bad");
-  expect(res.statusCode).toEqual(400);
-  expect(res.end).toHaveBeenCalledWith('{\n  "error": "bad"\n}');
+  test("error", async () => {
+    const { res } = getMockRes();
+    error(res, "bad");
+    expect(res.statusCode).toEqual(400);
+    expect(res.end).toHaveBeenCalledWith('{\n  "error": "bad"\n}');
+  });
 });
